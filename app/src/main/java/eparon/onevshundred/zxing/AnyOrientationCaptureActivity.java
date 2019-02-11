c375ae1426e8154e5f90527afdeb1e354896ed54
BuildTools <unconfigured@null.spigotmc.org>
2019-02-11 22:14:13 +0200

    Code optimizations. (v4.2.2-1)

--- app/src/main/java/eparon/onevshundred/zxing/AnyOrientationCaptureActivity.java
+++ app/src/main/java/eparon/onevshundred/zxing/AnyOrientationCaptureActivity.java
@@ -6,6 +6,4 @@ import com.journeyapps.barcodescanner.CaptureActivity;
  * This Activity is exactly the same as CaptureActivity, but has a different orientation
  * setting in AndroidManifest.xml.
  */
-public class AnyOrientationCaptureActivity extends CaptureActivity {
-
-}
+public class AnyOrientationCaptureActivity extends CaptureActivity { }
